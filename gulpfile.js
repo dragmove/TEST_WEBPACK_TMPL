e5aa71d2d0f13137091904f777d1bcfca1531f24
김현석 <dragmove@naver.com>
2017-01-25 17:42:10 +0900

    .merge.js has sourceMap strings. .min.js has external sourceMap file.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 /*
  * node_modules
  */
-//var sourcemaps = require('gulp-sourcemaps');
 var pkg = require('./package.json'),
   gulp = require('gulp'),
   rename = require("gulp-rename"),
@@ -30,18 +29,21 @@ function banner(_net) {
 
 function buildMergeJS(name, options) {
   var entry = {};
+  entry[name] = ['./app/' + name + '.js'];
 
-  if(options) {
-    if(options.hasNoBabelPolyfill) {
-      entry[name] = ['./app/' + name + '.js'];
-    }
-  } else {
-    entry[name] = ['babel-polyfill', './app/' + name + '.js'];
+  var dist = 'build';
+
+  if (options) {
+    if (options.requireBabelPolyfill === true) entry[name].unshift('babel-polyfill');
+    if (options.distPath) dist = options.distPath;
   }
 
   var config = extend({}, require('./webpack.config.js'), {
     devtool: 'eval-source-map',
     entry: entry,
+    output: {
+      filename: name + '.merge.js'
+    },
     plugins: [
       new webpack.optimize.UglifyJsPlugin({
         compress: {
@@ -54,44 +56,52 @@ function buildMergeJS(name, options) {
     ]
   });
 
-  var dist = (options && options.distPath) ? options.distPath : 'build';
   return gulp.src('')
     .pipe(webpackStream(config))
-    .pipe(concat( name + '.merge.js'))
-    .pipe(header( banner('dev') ))
-    .pipe(gulp.dest( dist ));
+    .pipe(header(banner('dev')))
+    .pipe(gulp.dest(dist));
 };
 
 function buildMinJs(name, options) {
   var entry = {};
-  if(options) {
-    if(options.hasNoBabelPolyfill) {
-      entry[name] = ['./app/' + name + '.js'];
-    }
-  } else {
-    entry[name] = ['babel-polyfill', './app/' + name + '.js'];
+  entry[name] = ['./app/' + name + '.js'];
+
+  var dist = 'build';
+
+  if (options) {
+    if (options.requireBabelPolyfill === true) entry[name].unshift('babel-polyfill');
+    if (options.distPath) dist = options.distPath;
   }
 
   var config = extend({}, require('./webpack.config.js'), {
     entry: entry,
+    output: {
+      filename: name + '.min.js'
+    },
     plugins: [
       new webpack.optimize.UglifyJsPlugin({
         compress: {
           drop_console: true,
           warnings: false
         },
-        sourceMap: false,
+        // make external sourcemap file
+        sourceMap: true,
         mangle: true
+      }),
+
+      // http://webpack.github.io/docs/list-of-plugins.html#sourcemapdevtoolplugin
+      new webpack.SourceMapDevToolPlugin({
+        test: [dist + '/' + name + '.min.js'],
+        filename: '[file].map',
+        append: '#sourceMappingURL'
       })
     ]
   });
 
-  var dist = (options && options.distPath) ? options.distPath : 'build';
   return gulp.src('')
     .pipe(webpackStream(config))
-    .pipe(concat( name + '.min.js' ))
-    .pipe(header( banner('live') ))
-    .pipe(gulp.dest( dist ));
+    .pipe(header(banner('live')))
+    .pipe(gulp.dest(dist));
 };
 
 
@@ -99,25 +109,33 @@ function buildMinJs(name, options) {
  * gulp tasks
  */
 // after run "gulp webpack-dev-server" in cmd window, connect "http://localhost:9001/webpack-dev-server/main.html"
-gulp.task('webpack-dev-server', function() {
+gulp.task('webpack-dev-server', function () {
   var config = require('./webpack.config.js'),
-    compiler  = webpack(config);
+    compiler = webpack(config);
 
-  var server = new WebpackDevServer( compiler , config.devServer );
-  server.listen(config.devServer.port, 'localhost', function(err) {
-    if(err) console.error('[webpack-dev-server failed to start :', err);
+  var server = new WebpackDevServer(compiler, config.devServer);
+  server.listen(config.devServer.port, 'localhost', function (err) {
+    if (err) console.error('[webpack-dev-server failed to start :', err);
   });
 });
 
-gulp.task('lint', function() {
+gulp.task('lint', function () {
   return gulp.src('app/*.js')
     .pipe(jshint())
     .pipe(jshint.reporter('default'));
 });
 
 // build js
-gulp.task('devMain', () => { buildMergeJS('main', {hasNoBabelPolyfill: true}) });
-gulp.task('liveMain', () => { buildMinJs('main', {hasNoBabelPolyfill: true}) });
+gulp.task('devMain', () => {
+  buildMergeJS('main', {requireBabelPolyfill: true})
+});
+gulp.task('liveMain', () => {
+  buildMinJs('main', {requireBabelPolyfill: true})
+});
 
-gulp.task('devSub', () => { buildMergeJS('sub', {hasNoBabelPolyfill: true}) });
-gulp.task('liveSub', () => { buildMinJs('sub', {hasNoBabelPolyfill: true}) });
+gulp.task('devSub', () => {
+  buildMergeJS('sub', {requireBabelPolyfill: true})
+});
+gulp.task('liveSub', () => {
+  buildMinJs('sub', {requireBabelPolyfill: true})
+});
